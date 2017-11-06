754b26cbd01200521815fa75f165628106e49024
David <davidsg1717@gmail.com>
2017-11-06 10:00:41 -0200

    First commit

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  render() {
+    const { className, ...props } = this.props;
+
+    return (
+      <div className={classnames('App', className)} {...props}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
