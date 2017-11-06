47f12055302f36a9706d31e00d30e8e5c01babce
David <davidsg.dev@gmail.com>
2017-11-06 11:29:54 -0200

    fix router

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 // src/routes.js
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import App from './components/App';
+import App from './components/App/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
 
 const Routes = (props) => (
-  <Router {...props}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
-    <Route path="*" component={NotFound} />
-  </Router>
+    <Router {...props}>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/about" component={About} />
+            <Route path="*" component={NotFound} />
+        </Switch>
+    </Router>
 );
 
 export default Routes;
